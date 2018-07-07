4a6dd425fb55b895ab21a87f3e7a82139a30dd81
Jakob Hermann Preusker <jakohe@outlook.de>
2018-07-07 23:50:59 +0200

    added app protocol (URI)

--- FastMD/MainPage.xaml.cs
+++ FastMD/MainPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
